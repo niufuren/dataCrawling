6a151326ef5aa4dc67b3ba4087ee172829822956
Juan Lu <juanlu.uom@gmail.com>
2016-05-02 17:04:26 +1000

    continuous page browsing available

--- dataCrawling.js
+++ dataCrawling.js
@@ -26,6 +26,90 @@ var pwd = 'projectmate2015';
 
 var getData = require('./getData');
 
+var pageNum = 1; 
+
+function processPage(){
+    //   this.wait(60000);
+    this.capture('afterSearch.png');
+    fs.write('afterSearch.html', this.getPageContent(), 'w');
+
+    // get the number of properties in a webpage 
+    //var hrefAll;
+
+    var propertyNum = this.evaluate(function() {
+        var searchList = document.querySelector('div[class="property-list"]');
+        var propertyItems = searchList.querySelectorAll('div[class="ng-scope"][ng-repeat="property in properties"]');
+        var propertyNum = propertyItems.length;
+
+        //var hrefAll = searchList.querySelectorAll('a[href]');
+
+        return propertyNum;
+    });
+
+    // this.echo(propertyNum, 'INFO');
+    var hrefStrPre = '#topOfSearchResults div div div:nth-child(';
+    var hrefStrPost = ') div.property-list-item.ng-isolate-scope div.property-list-item-body div.property-item-details a span.suburb-state-postcode.ng-binding';
+
+    //propertyNum=6;
+    var i;
+    var count = 1;
+    for (i = 1; i <= propertyNum; i++) {
+        var hrefStr = hrefStrPre + i + hrefStrPost;
+
+        // this.wait(10000, function() {
+        this.thenClick(hrefStr, function() {
+            this.wait(20000,
+                function then() {
+                    this.capture(count + '.png');
+                    fs.write(count + '-page.html', this.getPageContent(), 'w');
+                    fs.write(count + '.html', this.getHTML('div[name="propertyDetails"]', true), 'w');
+                    var propertyDetails;
+
+                    propertyDetails = this.evaluate(getData.getData);
+                    //this.echo(JSON.stringify(propertyDetails), 'INFO');
+                    this.echo(count, 'INFO');
+                    fs.write('2131-' + count + '.json', JSON.stringify(propertyDetails), 'w');
+                    count = count + 1;
+                });
+        });
+    }
+
+    var nextPage;
+    nextPage = 'a[class="paginator-button paginator-button-next"]'; // the button of next page 
+
+    if (this.exist(nextPage)){
+        var nextPageNum = pageNum+1;
+        this.thenClick(nextPage, processPage(nextPageNum));
+    }else{
+        this.echo("Done", "INFO").exit();
+    }
+
+};
+
+var processPage2=function(){
+    this.echo("**********");
+    this.capture('afterSearch-'+ pageNum+'.png');
+    fs.write('afterSearch-'+pageNum+'.html', this.getPageContent(), 'w');
+    var nextPagebutton;
+    nextPagebutton = 'a[class="paginator-button paginator-button-next"]'; // the button of next page 
+
+
+
+    if (this.exists(nextPagebutton)){
+        pageNum = pageNum+1;
+        this.echo(pageNum, 'INFO');
+        this.thenClick(nextPagebutton);
+        this.wait(10000);
+        this.then(function(){ 
+            this.waitForSelector('div[class="search-container"]',processPage2);
+        });
+    }else{
+        this.echo("Done", "INFO").exit();
+    }
+
+
+};
+
 //console.log(getData.answer1());
 
 casper.start(url);
@@ -95,80 +179,75 @@ casper.then(function() {
     //this.wait(60000);
 });
 
-casper.then(function() {
+casper.waitForSelector('div[class="search-container"]', processPage2);
+
+// casper.then(function() {
+//     var pageNum = 1; 
+    
+//     processPage(pageNum);
     //   this.wait(60000);
-    this.capture('afterSearch.png');
-    fs.write('afterSearch.html', this.getPageContent(), 'w');
+    // this.capture('afterSearch.png');
+    // fs.write('afterSearch.html', this.getPageContent(), 'w');
 
-    // get the number of properties in a webpage 
-    var hrefAll;
+    // // get the number of properties in a webpage 
+    // var hrefAll;
 
-    propertyNum = this.evaluate(function() {
-        var searchList = document.querySelector('div[class="property-list"]');
-        var propertyItems = searchList.querySelectorAll('div[class="ng-scope"][ng-repeat="property in properties"]');
-        var propertyNum = propertyItems.length;
+    // propertyNum = this.evaluate(function() {
+    //     var searchList = document.querySelector('div[class="property-list"]');
+    //     var propertyItems = searchList.querySelectorAll('div[class="ng-scope"][ng-repeat="property in properties"]');
+    //     var propertyNum = propertyItems.length;
 
-        //var hrefAll = searchList.querySelectorAll('a[href]');
+    //     //var hrefAll = searchList.querySelectorAll('a[href]');
 
-        return propertyNum;
-    });
+    //     return propertyNum;
+    // });
 
-    // this.echo(propertyNum, 'INFO');
-    var hrefStrPre = '#topOfSearchResults div div div:nth-child(';
-    var hrefStrPost = ') div.property-list-item.ng-isolate-scope div.property-list-item-body div.property-item-details a span.suburb-state-postcode.ng-binding';
+    // // this.echo(propertyNum, 'INFO');
+    // var hrefStrPre = '#topOfSearchResults div div div:nth-child(';
+    // var hrefStrPost = ') div.property-list-item.ng-isolate-scope div.property-list-item-body div.property-item-details a span.suburb-state-postcode.ng-binding';
 
-    //propertyNum=6;
-    var i;
-    var count = 1;
-    for (i = 1; i <= propertyNum; i++) {
-        var hrefStr = hrefStrPre + i + hrefStrPost;
+    // //propertyNum=6;
+    // var i;
+    // var count = 1;
+    // for (i = 1; i <= propertyNum; i++) {
+    //     var hrefStr = hrefStrPre + i + hrefStrPost;
 
+    //     // this.wait(10000, function() {
+    //     this.thenClick(hrefStr, function() {
+    //         // this.waitFor(
+    //         //     function check(){
+    //         //     return this.evaluate(function(){
+    //         //         var content = document.querySelector('div[name="propertyDetails"]');
+    //         //         var contentLength = content.innerText.length;
 
-//     casper.waitFor(function check() {
-//     return this.evaluate(function() {
-//         return document.querySelectorAll('ul.your-list li').length > 2;
-//     });
-// }, function then() {
-//     this.captureSelector('yoursitelist.png', 'ul.your-list');
-// });
+    //         //         return contentLength > 1;
+    //         //         });
+    //         //     //return true; 
+    //         //     }, 
+    //         this.wait(20000,
+    //             function then() {
+    //                 this.capture(count + '.png');
+    //                 fs.write(count + '-page.html', this.getPageContent(), 'w');
+    //                 fs.write(count + '.html', this.getHTML('div[name="propertyDetails"]', true), 'w');
+    //                 var propertyDetails;
 
-        // this.wait(10000, function() {
-        this.thenClick(hrefStr, function() {
-            // this.waitFor(
-            //     function check(){
-            //     return this.evaluate(function(){
-            //         var content = document.querySelector('div[name="propertyDetails"]');
-            //         var contentLength = content.innerText.length;
-
-            //         return contentLength > 1;
-            //         });
-            //     //return true; 
-            //     }, 
-            this.wait(20000,
-                function then() {
-                    this.capture(count + '.png');
-                    fs.write(count + '-page.html', this.getPageContent(), 'w');
-                    fs.write(count + '.html', this.getHTML('div[name="propertyDetails"]', true), 'w');
-                    var propertyDetails;
-
-                    propertyDetails = this.evaluate(getData.getData);
-                    //this.echo(JSON.stringify(propertyDetails), 'INFO');
-                    this.echo(count, 'INFO');
-                    fs.write('2131-' + count + '.json', JSON.stringify(propertyDetails), 'w');
-                    count = count + 1;
-                    //i++;
-                    //this.wait(60000);
-                }
-                // ,
-                // function timeout() { // step to execute if check has failed
-                //     this.echo("Can't load page", 'ERROR');
-                // },
-                // 60000 
-            );
-        });
-    }
-
-});
+    //                 propertyDetails = this.evaluate(getData.getData);
+    //                 //this.echo(JSON.stringify(propertyDetails), 'INFO');
+    //                 this.echo(count, 'INFO');
+    //                 fs.write('2131-' + count + '.json', JSON.stringify(propertyDetails), 'w');
+    //                 count = count + 1;
+    //                 //i++;
+    //                 //this.wait(60000);
+    //             }
+    //             // ,
+    //             // function timeout() { // step to execute if check has failed
+    //             //     this.echo("Can't load page", 'ERROR');
+    //             // },
+    //             // 60000 
+    //         );
+    //     });
+    // }
+//});
 
 
 casper.run(
